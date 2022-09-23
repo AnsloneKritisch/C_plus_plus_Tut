f541bbd64794d7bc23fa136b8a22eb8fed224bb6
AnsloneKritisch <digveshoptimus@gmail.com>
2022-09-23 12:42:17 +0530

    new

---
+++ 01_Basic/01_print.c++
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+using namespace std ;
+// Just to avoid using std every time using cout , cin
+
+ int main()
+ {
+    cout<<"Hello World" ;
+
+    getch();
+    return 0;
+ }
+ 
---
+++ 01_Basic/02_variables.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <conio.h>
+using namespace std ;
+
+ int main()
+ {
+    int a , b , c ;
+    cout<<"Enter 1st number : ";
+    cin>>a ;
+    
+    cout<<"Enter 2nd number : ";
+    cin>>b ;
+
+    c = a + b ;
+
+    cout<<"Sum of a and b is "<<c;
+
+    
+
+    getch();
+    return 0;
+ }
+ 
