13ec761140eb8e5d9234d23d7197bf1ebb555e41
AnsloneKritisch <digveshoptimus@gmail.com>
2022-11-29 10:27:29 +0530

    new

---
+++ 03_Control_Structure/02_loops/01_for-loop/01_basic.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+  int i ;
+
+  for ( i =0 ; i<=10 ; i++)
+  {
+    cout << i << "\n";
+  }
+
+  getch();  
+  return 0;
+}
